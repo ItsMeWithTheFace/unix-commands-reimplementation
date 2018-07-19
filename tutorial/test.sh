64482e84f22613d7f05629720b33ece539db8ff2
Stephen Luc <stephenluc@users.noreply.github.com>
2018-07-19 18:55:30 -0400

    Update test.sh

--- tutorial/test.sh
+++ tutorial/test.sh
@@ -1,8 +1,56 @@
 gcc readimage.c -Wall
-echo "emptydisk"
-./a.out ../images/emptydisk.img
-echo "twolevel"
-./a.out ../images/twolevel.img
-echo "onefile"
-./a.out ../images/onefile.img
 
+image1 = $1
+image2 = $2
+image3 = $3
+image4 = $4
+image5 = $5
+image6 = $6
+
+if [ -n image1  ]
+then
+    echo "========================"
+    echo $1
+    echo "========================"
+    ./a.out ../images/$1
+else
+    echo "========================"
+    echo "emptydisk"
+    echo "========================"
+    ./a.out ../images/emptydisk.img
+
+    echo "========================"
+    echo "onefile"
+    echo "========================"
+    ./a.out ../images/onefile.img
+
+    echo "========================"
+    echo "deletedfile"
+    echo "========================"
+    ./a.out ../images/deletedfile.img
+
+    echo "========================"
+    echo "onedirectory"
+    echo "========================"
+    ./a.out ../images/onedirectory.img
+
+    echo "========================"
+    echo "hardlink"
+    echo "========================"
+    ./a.out ../images/hardlink.img
+
+    echo "========================"
+    echo "deleteddirectory"
+    echo "========================"
+    ./a.out ../images/deleteddirectory.img
+
+    echo "========================"
+    echo "twolevel"
+    echo "========================"
+    ./a.out ../images/twolevel.img
+
+    echo "========================"
+    echo "largefile"
+    echo "========================"
+    ./a.out ../images/largefile.img
+  fi
